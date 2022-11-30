bad3b0dac899687ca02ee991d89d033e019c0d0f
Kauã de Oliveira <98750284+kauadeoliveira@users.noreply.github.com>
2022-11-30 18:53:20 -0300

    type for functions

--- ts-basics/functions.ts
+++ ts-basics/functions.ts
@@ -6,3 +6,14 @@ export function some(a: number, b: number): number {
 export function greet(greeting: string, ...names: string[]): string {
     return `${greeting} ${names.join(', ')} !`
 }
+
+type MutationFunction = (value: number) => number;
+
+export function arrayFunction(numbers: number[], callback: MutationFunction): number[] {
+    return numbers.map(callback)
+}
+
+type CreateAdderFunction = (v: number) => number
+export function createAdder(num: number): CreateAdderFunction {
+    return (val: number) => num + val
+}
