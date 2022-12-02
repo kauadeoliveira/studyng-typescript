bbb5ddac6b79a43fa197dc898f34cee5b89b5e74
Kauã de Oliveira <98750284+kauadeoliveira@users.noreply.github.com>
2022-12-02 08:38:46 -0300

    data types

---
+++ ts-basics/data types/index.ts
@@ -0,0 +1,14 @@
+// string
+const exampleStr: string = 'Hello';
+
+// number
+const exampleNum: number = 10;
+
+// array
+const exampleArrayStr: string[] = ['A', 'E', 'I', 'O', 'U'];
+const exampleArrayNum: number[] = [10, 20, 30, 40, 50];
+
+// tuple
+const exampleTuple1: [number, string, string] = [10, 'Neymar Jr', 'Brasil'];
+const exampleTuple2: [number, string, string][] = [[10, 'Neymar Jr', 'Brasil'], [10, 'Messi', 'Argentina'], [7, 'CR7', 'Portugal']];
+const exampleTuple3: [string, boolean] = ['Neymar Jr', true]
