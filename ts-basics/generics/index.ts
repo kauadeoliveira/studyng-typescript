53ba4a8710fdb5a928b6f8959f583d7fbbc3720a
Kauã de Oliveira <98750284+kauadeoliveira@users.noreply.github.com>
2022-12-05 11:53:43 -0300

    generics ts

---
+++ ts-basics/generics/index.ts
@@ -0,0 +1,11 @@
+
+// Generic function
+const example1 = <T> (param: T): void => {
+    console.log(param)
+}
+
+// Multiple Type Variables
+const example2 = <T, U> (a: T, b: U): void => {
+    console.log(a)
+    console.log(b)
+}
