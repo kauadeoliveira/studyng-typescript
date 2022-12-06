9f855bfae0f2fb6fcd6efb77f87191b196b42da8
Kauã de Oliveira <98750284+kauadeoliveira@users.noreply.github.com>
2022-12-06 13:40:39 -0300

    solutions challenge 2

--- ts-basics/challenges/challenge2/index.js
+++
@@ -1,6 +0,0 @@
-const array = [1, 2, 3, 4, 5];
-
-console.log(array.forEach((num) => num*2))
-console.log('array: ', array)
-
-
--- ts-basics/challenges/challenge2/index.ts
+++ ts-basics/challenges/challenge2/index.ts
@@ -1,25 +1,20 @@
-const arrayTeste = [1, 2, 3, 4, 5]
+const arrayTeste = [9, 2, 3, 4, 5]
 
 function myFilter<T>(array: T[], callback: (value: T) => boolean) : T[] {
     const myArray: T[] = [];
 
-    array.reduce((accumulator: T, currentValue: T): any => callback(currentValue) ? myArray.push(currentValue) : false)
+    array.reduce((accumulator, currentValue): any => callback(currentValue) ? myArray.push(currentValue) : false, 0)
 
     return myArray
 }
 
 function myMap<T>(array: T[], callback: (value: T) => any): T[] {
     const myArray: T[] = []
-    for(let i: number = 0; array.length > i; i++){
-        myArray.push(callback(array[i]))
-    }
+    array.reduce((accumulator, currentValue): any => myArray.push(callback(currentValue)), 0)
+
     return myArray
 }
 
-function myForEach<T>(array: T[], callback: (value: T) => any): any {
-    for(let i: number = 0; array.length > i; i++) {
-        callback(array[i])
-    }
+function myForEach<T>(array: T[], callback: (value: T) => void): void {
+    array.reduce((accumulator, currentValue): any => callback(currentValue), 0)
 }
-
-myForEach(arrayTeste, (value) => console.log(value))
