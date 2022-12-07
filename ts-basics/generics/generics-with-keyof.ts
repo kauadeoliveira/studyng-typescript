1e9224a5ff27d76be5d2abb01ae4752ce3ac3825
Kauã de Oliveira <98750284+kauadeoliveira@users.noreply.github.com>
2022-12-07 08:25:17 -0300

    generics with keyof

--- ts-basics/generics/generics-with-keyof.ts
+++ ts-basics/generics/generics-with-keyof.ts
@@ -7,4 +7,18 @@ function getProperty<T extends object, K extends keyof T>(obj: T, key: K) {
     return obj[key]
 }
 
-console.log(getProperty(user, 'id'))
+// console.log(getProperty(user, 'id'))
+
+
+
+
+function pluck<DataType extends object, KeyType extends keyof DataType>(items: DataType[], key: KeyType){
+    return items.map(item => item[key])
+}
+
+const dogs = [
+    {name: 'katito', age: 10},
+    {name: 'luna', age: 3}
+]
+
+console.log(pluck(dogs, 'name'))
