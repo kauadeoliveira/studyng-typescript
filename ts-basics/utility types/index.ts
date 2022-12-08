9f89754f9f390c8908f265000ab516b54fe6b191
Kauã de Oliveira <98750284+kauadeoliveira@users.noreply.github.com>
2022-12-08 06:40:28 -0300

    utility types: pick

--- ts-basics/utility types/index.ts
+++ ts-basics/utility types/index.ts
@@ -6,13 +6,13 @@ interface Dog {
 }
 
 // Partial
-const Katito: Partial<Dog> = {
+const katito: Partial<Dog> = {
     name: 'katito',
     color: 'caramelo' 
 } 
 
 // Required
-const Luna: Required<Dog> = {
+const luna: Required<Dog> = {
     name: 'Luna',
     breed: 'Shitzu',
     age: 3,
@@ -20,7 +20,7 @@ const Luna: Required<Dog> = {
 }
 
 // Readonly
-const Yummi: Readonly<Dog> = {
+const yummi: Readonly<Dog> = {
     name: 'Yummi',
     breed: 'Shitzu',
     color: 'White'
@@ -35,9 +35,26 @@ interface Person {
 
 type PersonNames = "Kauã" | "Joice" | "Lorena"
 
-const Peoples: Record<PersonNames, Person> = {
+const peoples: Record<PersonNames, Person> = {
     Joice: { name: "Joice Oliveira", age: 37, country: "Brasil" },
     Kauã: { name: "Kauã Oliveira", age: 19, country: "Brasil" },
     Lorena: { name: "Lorena Oliveira", age: 1, country: "Brasil" }
 }
 
+// Pick
+interface Vehicle {
+    type: string
+    model: string
+    rodas: number
+    portas: number
+    fullSpeed: number
+}
+
+type Moto = Pick<Vehicle, "type" | "model" | "rodas" | "fullSpeed">
+
+const hornet600: Moto = {
+    type: "Moto",
+    model: "Hornet CB 600",
+    rodas: 2,
+    fullSpeed: 200
+}
